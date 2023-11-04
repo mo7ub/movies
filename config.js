756d5b6d287880eddec380fbca80fb73f8956fc5
mo7ub <56566800+mo7ub@users.noreply.github.com>
2023-11-04 20:19:43 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
-  VITE_CORS_PROXY_URL: "test.mohamedgamal657.workers.dev",
+  VITE_CORS_PROXY_URL: "https://test.mohamedgamal657.workers.dev",
   VITE_TMDB_READ_API_KEY: ""
 };
